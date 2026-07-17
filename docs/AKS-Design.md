6ab26bf6d7cdfea45349048f35ade42607832e45
gunjankapoor99 <darklegend1931@gmail.com>
2026-07-17 16:11:07 +0000

    feat(terraform): enable AKS production add-ons and autoscaling

---
+++ docs/AKS-Design.md
@@ -0,0 +1,26 @@
+# AKS Design Decisions
+
+## Cluster
+- SKU: Free
+- Region: Central India
+- Kubernetes Version: <to be determined>
+
+## Networking
+- Azure CNI Overlay
+
+## Identity
+- System Assigned Managed Identity
+
+## Monitoring
+- Azure Monitor
+- Log Analytics Workspace
+
+## Security
+- Azure RBAC
+- OIDC Issuer
+- Workload Identity
+
+## Node Pool
+- Standard_B2s
+- 1 Node
+- System Node Pool
--- terraform/modules/aks/main.tf
+++ terraform/modules/aks/main.tf
@@ -10,31 +10,61 @@ resource "azurerm_kubernetes_cluster" "this" {
 
   sku_tier = "Free"
 
+  # Production-grade identity features
+  # oidc_issuer_enabled       = true
+  # workload_identity_enabled = true
+
+  role_based_access_control_enabled = true
+
+  # azure_active_directory_role_based_access_control {
+  #  azure_rbac_enabled = true
+  # }
+
   default_node_pool {
 
     name = "system"
 
-    node_count = var.node_count
-
     vm_size = var.vm_size
 
+    node_count = 1
+
     vnet_subnet_id = var.subnet_id
-  }
-  
-  network_profile {
-    network_plugin = "azure"
-    network_plugin_mode = "overlay"
 
-    service_cidr = "172.16.0.0/16"
-    dns_service_ip = "172.16.0.10"
+    os_disk_size_gb = 64
 
-    load_balancer_sku = "standard"
+    type = "VirtualMachineScaleSets"
+
+    #auto_scaling_enabled = true
+    #min_count            = 1
+    #max_count            = 3
+
+    node_labels = {
+      "nodepool-type" = "system"
+      "environment"   = "dev"
+    }
   }
 
   identity {
     type = "SystemAssigned"
   }
 
+  network_profile {
+
+    network_plugin      = "azure"
+    network_plugin_mode = "overlay"
+
+    network_policy = "azure"
+
+    service_cidr   = "172.16.0.0/16"
+    dns_service_ip = "172.16.0.10"
+
+    load_balancer_sku = "standard"
+  }
+
+  #oms_agent {
+
+   # log_analytics_workspace_id = var.log_analytics_workspace_id
+  #}
+
   tags = var.tags
 }
-
